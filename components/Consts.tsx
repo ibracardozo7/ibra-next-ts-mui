b94606d28e731c4706bd432dd0e192cc95afdeef
ibracardozo7 <ibracardozo15@gmail.com>
2022-12-02 19:09:16 -0300

    avance portfolio

--- components/Consts.tsx
+++ components/Consts.tsx
@@ -2,11 +2,6 @@ import {
     SiHtml5,
     SiCss3,
     SiJavascript,
-    SiGit,
-    SiGithub,
-    SiWhatsapp,
-    SiInstagram,
-    SiLinkedin,
     SiNodedotjs,
     SiReact,
     SiRedux,
@@ -14,13 +9,8 @@ import {
     SiNextdotjs,
     SiPostgresql,
     SiTypescript,
-    SiSass,
-    SiStyledcomponents,
-    SiVuedotjs,
     SiExpress,
     SiSequelize,
-    SiQuasar,
-    SiGraphql
 } from 'react-icons/si';
 
 export const aboutMe = [
@@ -29,7 +19,6 @@ export const aboutMe = [
     'Amante de la programación, disfruto mucho trabajar en equipo.',
     'Puedo aportar soluciones óptimas en poco tiempo.',
     'Me encuentro reforzando conocimientos en Next.js y Material UI.',
-    // 'Estoy abierto a la relocalización.',
 ]
 
 export const skill = [
@@ -38,18 +27,74 @@ export const skill = [
     { name: 'JavaScript', icon: <SiJavascript />, url: 'https://developer.mozilla.org/es/docs/Web/JavaScript' },
     { name: 'TypeScript', icon: <SiTypescript />, url: 'https://www.typescriptlang.org/' },
     { name: 'React js', icon: <SiReact />, url: 'https://reactjs.org/' },
-    // { name: 'React Native', icon: <SiReact />, url: 'https://reactnative.dev/' },
     { name: 'Redux js', icon: <SiRedux />, url: 'https://redux.js.org/' },
     { name: 'Material UI', icon: <SiMaterialui />, url: 'https://mui.com/' },
-    // { name: 'SASS', icon: <SiSass />, url: 'https://sass-lang.com/' },
-    // { name: 'Styled Components', icon: <SiStyledcomponents />, url: 'https://styled-components.com/' },
     { name: 'Next js', icon: <SiNextdotjs />, url: 'https://nextjs.org/' },
-    // { name: 'Vue js', icon: <SiVuedotjs />, url: 'https://vuejs.org/' },
-    // { name: 'Quasar', icon: <SiQuasar />, url: 'https://quasar.dev/' },
     { name: 'Node js', icon: <SiNodedotjs />, url: 'https://nodejs.org/' },
     { name: 'Express js', icon: <SiExpress />, url: 'https://expressjs.com/' },
-    // { name: 'GraphQL', icon: <SiGraphql />, url: 'https://graphql.org/' },
     { name: 'PostgreSQL', icon: <SiPostgresql />, url: 'https://www.postgresql.org/' },
     { name: 'Sequelize', icon: <SiSequelize />, url: 'https://sequelize.org/' },
-    // { name: 'Git', icon: <SiGit />, url: 'https://git-scm.com/' },
+]
+
+interface Project {
+    title: string;
+    description: string;
+    image: string;
+    image2?: string;
+    languages: string[];
+    demo: string;
+    github: string;
+}
+
+export const myProjects: Project[] = [
+    {
+        title: 'Portafolio',
+        description: `Portafolio de proyectos personal en el cual puedes ver mis trabajos y proyectos en los cuales he trabajado, fue desarrollado con tecnologías que fui aprendiendo y reforzando a la par.`,
+        image: '/port.png',
+        image2: '/port2.png',
+        languages: ['React.js','Next.js', 'TypeScript', 'Material UI'],
+        demo: '',
+        github: 'https://github.com/ibracardozo7/ibra-next-ts-mui',
+    },
+    {
+        title: 'Gestios de Negocio: Liberfit gym',
+        description: `Desarrollo de una Aplicación usando SCRUM y aplicando las tecnologías aprendidas durante el 
+        cursado del Bootcamp. Desarrollé mis actividades como Frontend Developer, usando Tailwind CSS para la estilización de sus componentes.`,
+        image: '',
+        languages: ['React.js', 'Redux.js', 'Tailwind CSS', 'Auth0'],
+        demo: 'https://gym-mthatwords.vercel.app/',
+        github: 'https://github.com/ibracardozo7/LiberFit-PF',
+    },
+    {
+        title: 'Proyecto Individual: Dogs App',
+        description: `Aplicación creada con React.js, Redux y CSS en la cual puedes buscar razas de perros que estén disponibles en la API, ver sus detalles e imágenes, filtrarlos por tipo de razas y ordenarlos de distintas maneras.`,
+        image: '',
+        languages: [
+            'React.js',
+            'Redux.js',
+            'CSS',
+            'Node.js',
+            'Express.js',
+            'Sequelize',
+            'PostgreSQL',
+        ],
+        demo: '',
+        github: 'https://github.com/ibracardozo7/PI-Dogs-main',
+    },
+    {
+        title: 'Proyecto Individual: Pokemons App',
+        description: `Aplicación creada con React.js, Redux para el Frontend. Esta aplicación consume datos de una base de datos que fueron cargados desde una API a través de un Backend creado con Express. Y sirve para buscar y mostrar datos de algún Pokémon y poder crear.`,
+        image: '',
+        languages: [
+            'React.js',
+            'Redux.js',
+            'CSS',
+            'Node.js',
+            'Express.js',
+            'Sequelize',
+            'PostgreSQL',
+        ],
+        demo: '',
+        github: 'https://github.com/ibracardozo7/PI-Pokemon-main',
+    }
 ]
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,8 +1,63 @@
+import { Container, Grid, Box, useTheme } from '@mui/material'
+import Project from '../components/Proyect'
+import Title from '../components/Title'
+import { myProjects } from '../components/Consts'
+
 const Portfolio = () => {
+    const { palette } = useTheme()
+    const { mode } = palette
     return (
-        <>
-        portfolio Ibra
-        </>
+        <Box
+            component="section"
+            id="portfolio"
+            sx={{
+                minHeight: '100vh',
+                py: 10,
+                px: 2,
+            }}>
+            <Container>
+                <Grid
+                    container
+                    direction='column'
+                    spacing={5}>
+                    <Grid item xs={12}>
+                        <Title title="Portafolio" size={2.5} />
+                    </Grid>
+                    <Grid
+                        container
+                        item
+                        xs={12}
+                        spacing={8}>
+                        {
+                            myProjects.map((project, i) => (
+                                <Grid
+                                    key={project.title}
+                                    item
+                                    xs={12}
+                                    md={6}
+                                >
+                                    <Project
+                                        description={project.description}
+                                        image={
+                                            project.image2 ?
+                                                mode === 'dark'
+                                                    ? project.image2
+                                                    : project.image
+                                                : project.image
+                                        }
+                                        languages={project.languages}
+                                        title={project.title}
+                                        direction={i % 2 === 1 ? false : true}
+                                        demo={project.demo}
+                                        github={project.github}
+                                    />
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
+                </Grid>
+            </Container >
+        </Box>
     )
 }
 
